399a66aa3bf475d8534ab8691794d29043cd7800
Roshan <roshan.rm01@gmail.com>
2021-10-30 22:26:25 +0100

    added routes for Job

--- backend/models/organisation/model.py
+++ backend/models/organisation/model.py
@@ -1,23 +1,47 @@
-from odmantic import Field, Model
+from odmantic import Field, Model, EmbeddedModel
 from pydantic import EmailStr
 from typing import Optional
 
-
-class Organisation(Model):
-    id: int
-    firstName: str
-    lastName: str
-    email: EmailStr
-    password: str
-    companyName: str
+class OrganisationData(EmbeddedModel):
     location: str
     companyBio: str
     industry: str
     telephone: str
     companySize: Optional[str] = None
     
+    class Config:
+        scheme_extra = {
+            "example": {
+                "location": "USA",
+                "companyBio": "Google is the largest technology company",
+                "industry": "Technology",
+                "telephone": "+447398346125",
+                "companySize": "10,000+ Employees"
+            }
+        }
+
+
+class Organisation(Model):
+    firstName: str
+    lastName: str
+    email: EmailStr
+    password: str
+    companyName: str
+    data: Optional[OrganisationData]
+        
+    class Config:
+        scheme_extra = {
+            "example": {
+                "firstName": "Larry",
+                "lastName": "Page",
+                "email": "page@gmail.com",
+                "password": "Strong+Password",
+                "companyName": "Google"
+            }
+        }
+        
+    
 class Job(Model):
-    id: int
     title: str
     role: str
     description: str
@@ -25,4 +49,27 @@ class Job(Model):
     company: Organisation
     experience: str
     salary: float
-    
+        
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Software Engineer",
+                "role": "Junior",
+                "description": "Python developer needed to develop the Backend",
+                "industry": "Internet and Technology",
+                "company": { 
+                    "firstName": "Larry",
+                    "lastName": "Page",
+                    "email": "page@gmail.com",
+                    "password": "Strong+Password",
+                    "companyName": "Google",
+                    "location": "USA",
+                    "companyBio": "Google is the largest technology company",
+                    "industry": "Technology",
+                    "telephone": "+447398346125",
+                    "companySize": "10,000+ Employees"
+                },
+                    "experience": "0-2 years",
+                    "salary": 1500.00
+            }
+        }
--- backend/routes/organisation/route.py
+++ backend/routes/organisation/route.py
@@ -1,20 +1,63 @@
-from fastapi import FastAPI
-from backend.models.organisation.model import Job
-from backend.database.database_methods import Database
+from fastapi import APIRouter, Response, Depends, HTTPException, status, body
+from models.organisation.model import Job
+from database.database_methods import Database
 
-app = FastAPI()
+job_router = APIRouter()
+job_database = Database(Job)
 
-@app.post('/job/create/', response_model=Job)
-async def create_job(job: Job):
-    return job
+@job_router.post('/job/create/', response_model=Job)
+async def create_job(job_data: Job):
+    # if job exists
+    
+    job_exists = await job_database.find_one(Job._id == job_data._id)
+    if job_exists:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Job post exists"
+        )
+    await job_database.save(job_data)
+    return {
+        "action": "Job Create",
+        "message": "Job Post created"
+    }
 
-@app.get('/job/get/{job_id}/', response_model=Job)
+@job_router.get('/job/get/{job_id}/', response_model=Job)
 async def get_job(job_id: int):
-    return int
+    get_job = await job_database.find_one(Job._id == job_id)
+    if not get_job:
+        raise HTTPException(
+            status_code=404,
+            detail="Job don't exist"
+        )
+    return get_job
 
-@app.put('/job/update/{job_id}/', response_model=Job)
-async def update_job(job: Job):
-    return job
+@job_router.put('/job/update/{job_id}/', response_model=Job)
+async def update_job(job_id: int, job_data: Job):
+    get_job = await job_database.find_one(Job._id == job_id)
+    if not get_job:
+        raise HTTPException(
+            status_code=404,
+            detail="Job don't exist"
+        )
+    await job_database.save(job_data)
+    return {
+        "action": "Job Update",
+        "message": "Job Post updated"
+    }
 
-@app.delete('job/delete/{job_id}/'):
-    return {'model': 'deleted'}
+@job_router.delete('job/delete/{job_id}/', response_model=Job):
+async def delete_job(job_id: int):
+    get_job = await job_database.find_one(Job._id == job_id)
+    if not get_job:
+        raise HTTPException(
+            status_code=404,
+            detail="Job don't exist"
+        )
+    await job_database.delete(get_job)
+    return {
+        "action": "Job Delete",
+        "message": "Job Post deleted"
+    }
+
+
+
